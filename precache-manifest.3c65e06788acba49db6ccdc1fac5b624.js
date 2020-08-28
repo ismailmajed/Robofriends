904f34040c2df4e2baa72c7a169115ac542cfa15
Ismail <ismailmajed5@gmail.com>
2020-08-28 11:54:55 -0600

    Updates

---
+++ precache-manifest.3c65e06788acba49db6ccdc1fac5b624.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0a165a89d78dcd9f0093846ef0d0521a",
+    "url": "/Robofriends/index.html"
+  },
+  {
+    "revision": "a09f2fe49b1513e05f9e",
+    "url": "/Robofriends/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "96455647117ab13f4302",
+    "url": "/Robofriends/static/css/main.a40c60a6.chunk.css"
+  },
+  {
+    "revision": "a09f2fe49b1513e05f9e",
+    "url": "/Robofriends/static/js/2.498787a0.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Robofriends/static/js/2.498787a0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "96455647117ab13f4302",
+    "url": "/Robofriends/static/js/main.4af4fce7.chunk.js"
+  },
+  {
+    "revision": "a6103a62c81f6271b765",
+    "url": "/Robofriends/static/js/runtime-main.79cc7977.js"
+  }
+]);
